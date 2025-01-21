8f1888bdc6502de6ef4ae11869a24f6583b8159f
Raiyan Rahman <163680824+raiyanr6@users.noreply.github.com>
2025-01-21 17:15:00 +0800

    Add files via upload

---
+++ VirtualBazaarApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import com.example.demo.base.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBazaarApplication.class, args);
+
+
+	}
+
+}
